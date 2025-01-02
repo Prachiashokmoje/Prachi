04c9f10d4c5b90b939d79736646abb3e10643d34
Prachiashokmoje <prachimoje75@gmail.com>
2025-01-02 12:31:44 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,8 @@
+a = int(input("Entyer n1"))
+b = int(input("Entyer n2"))
+s = input("Enter Substraction(-) or Multiplication(*)")
+if s== '-':
+    print("The result is =", a-b)
+elif s== '*':
+    print("The result is =", a*b)
+
